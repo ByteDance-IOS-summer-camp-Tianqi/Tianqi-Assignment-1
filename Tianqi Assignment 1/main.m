ba41a2aa925accdca578d8a3aabf7273a881df03
Xiaojian Chen <lzokchen@ucla.edu>
2022-08-20 20:52:31 -0700

    Initial Commit

---
+++ Tianqi Assignment 1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tianqi Assignment 1
+//
+//  Created by Xiaojian Chen on 8/20/22.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
